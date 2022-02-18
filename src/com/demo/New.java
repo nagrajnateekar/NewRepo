a5cf6804d7c2876009f44cc9eae78baab587e301
nagrajnateekar <nagrajnateekar555@gmail.com>
2022-02-18 16:20:03 +0530

    this new file

--- src/com/demo/New.java
+++ src/com/demo/New.java
@@ -3,8 +3,9 @@ package com.demo;
 public class New {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		System.out.println("added new code");
+		System.out.println("new addition");
 	}
 
 }
